d235f6490dfeff5175d7bb3fd83ba0761159ac65
chamwiz <39915310+chamwiz@users.noreply.github.com>
2023-01-08 15:21:55 +0900

    Update ad.js

--- ad.js
+++ ad.js
@@ -7,7 +7,7 @@
 //지프텔
 let img_url = "https://raw.githubusercontent.com/chamwiz/chamwiz.github.io/main/giptal.jpg";
 let ad_link = "http://kerion.info/t7dsmg0d0z";
-let text_description = "당신의 지프텔 실력은? 무료 테스트.\n\n해커스 지프텔 인강 24시간 무료 수강권 + 할인 쿠폰 제공";
+let text_description = "당신의 지프텔 실력은? 무료 테스트.\n\n해커스 지프텔 인강 24시간 무료 수강권 + 할인 쿠폰 제공. 여러분들의 지프텔 실력을 테스트해보세요. 몇점이 나오나요?";
 
 document.getElementById("img_link").href = ad_link;
 document.getElementById("ad_img_p").src = img_url;
