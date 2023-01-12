1e361387100e40c572a182a81f6a8956c584014c
chamwiz <39915310+chamwiz@users.noreply.github.com>
2023-01-12 23:04:07 +0900

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,4 +1,4 @@
-let whichAd = 3;
+let whichAd = 1;
 
 let img_url = "";
 let ad_link = "";
@@ -16,9 +16,9 @@ if(whichAd == 1){
     text_description = "당신의 지프텔 실력은? 무료 테스트.\n\n해커스 지프텔 인강 24시간 무료 수강권 + 할인 쿠폰 제공. 여러분들의 지프텔 실력을 테스트해보세요. 몇점이 나오나요?";
 } else if(whichAd == 3){
 //댕냥룸
-img_url = "https://github.com/chamwiz/chamwiz.github.io/blob/main/cat.jpg?raw=true";
-ad_link = "http://kerion.info/t7dsnx31gz";
-text_description = "댕냥이 좋아하시는 분 주목!!!\n\n댕냥룸 콘테스트 참여하고 이벤트 경품 받으세요~ 비글지에서 가상 펫을 만들고 소중히 키우고 다른 사람들과 이야기를 나눠봐요. 여친, 이 생길지도?";
+img_url = "https://github.com/chamwiz/chamwiz.github.io/blob/main/clean.jpg?raw=true";
+ad_link = "http://kerion.info/t62fq98z3n";
+text_description = "청소매니아에서 무료 견적 받아보세요\n\n집, 회사, 공간 청소 하기 힘드시죠? 청소를 맡겨보세요. 국내 청소 최대 플랫폼인 최소매니아를 소개합니다.";
 }
 
 document.getElementById("img_link").href = ad_link;
